7a30d1f3a068d6e91edbf8dd05556a97e7facb05
hkminegod <hkminegod@users.noreply.github.com>
2015-04-06 15:27:08 +0800

    Cleanup and improve algorithm

--- src/in/choy/math/ia/Main.java
+++ src/in/choy/math/ia/Main.java
@@ -1,24 +1,54 @@
 package in.choy.math.ia;
 
 public class Main {
+    static long n = 938065379; /* integer for factorization */
+    static double aa = Math.ceil(Math.sqrt(n));
+    static long a = (long)aa;
+    static int ffLooped = 0;
+    static int bfLooped = 0;
+    static double bsquared = a * a - n;
+
+    public static boolean sqrtnisEven(double sqrtn) {
+        if (sqrtn%2==0)
+            return true;
+        return false;
+    }
+
+    public static boolean aIsEven() {
+        double nn = (long)n;
+        if((nn+1)%4==0)
+            return true;
+        return false;
+    }
+    public static void theLoop() {
+        while (Math.sqrt(bsquared)%1 != 0) {
+            a = a + 2; //increase sqrtn by 2
+            bsquared = a * a - n; //rerun algorithm
+            ffLooped++; //count the number of times calculated
+        }
+    }
 
     public static void main(String[] args) {
-
-        long n = 938065379; /* integer for factorization */
-        long sqrtn = 30628; /* square root of the integer for factorization */
-        int ffLooped = 0;
-        int bfLooped = 0;
-        long bsquared = sqrtn * sqrtn - n;
         final long ffstartTime = System.currentTimeMillis();
-        while (Math.sqrt(bsquared)%1 != 0) {
-            sqrtn = sqrtn+1; //increase sqrtn by 1
-            bsquared = sqrtn * sqrtn - n; //rerun algorithm
-            ffLooped++; //count the number of times calculated
+        if (aIsEven()) { //a is even
+            if(sqrtnisEven(a)) {
+                theLoop();
+            } else {
+                a++;
+                theLoop();
+            }
+        } else { //a is odd
+            if(sqrtnisEven(a)) {
+            a++;
+                theLoop();
+            } else {
+                theLoop();
+            }
         }
 
         final long ffendTime = System.currentTimeMillis();
-        System.out.println("FF: " + (sqrtn - (long)Math.sqrt(bsquared)) + " is a factor");
-        System.out.println("FF: " + (sqrtn + (long)Math.sqrt(bsquared)) + " is another factor");
+        System.out.println("FF: " + (a - (long)Math.sqrt(bsquared)) + " is a factor");
+        System.out.println("FF: " + (a + (long)Math.sqrt(bsquared)) + " is another factor");
         System.out.println("FF: Total execution time: " + (ffendTime - ffstartTime) + "ms" );
         System.out.println("FF: steps involved: " + ffLooped);
 
@@ -28,11 +58,11 @@ public class Main {
         while(!(n%i==0)) {
             i = i + 2;
             bfLooped++;
-            }
-            final long bfendTime = System.currentTimeMillis();
-            System.out.println("BF: " + i + " is a factor");
-                System.out.println("BF: " + n/i + " is another factor");
-                System.out.println("BF: Total execution time: " + (bfendTime - bfstartTime) + "ms" );
-                System.out.println("BF: steps involved: " + bfLooped);
         }
+        final long bfendTime = System.currentTimeMillis();
+        System.out.println("BF: " + i + " is a factor");
+        System.out.println("BF: " + n/i + " is another factor");
+        System.out.println("BF: Total execution time: " + (bfendTime - bfstartTime) + "ms" );
+        System.out.println("BF: steps involved: " + bfLooped);
     }
+}
